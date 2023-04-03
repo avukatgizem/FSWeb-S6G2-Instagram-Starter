3c8ea01dcc2cdc1cbfedcddfafb68aee4c995157
avukatgizem <125642428+avukatgizem@users.noreply.github.com>
2023-04-04 00:05:37 +0300

    son

--- src/App.js
+++ src/App.js
@@ -5,55 +5,74 @@
 */
 
 // State hook u import edin
-import React, {useState} from 'react';
+import React from "react";
+import { useState } from "react";
+
+// Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
+// sahteVeri'yi import edin
 import "./App.css";
 import AramaÇubuğu from "./bileşenler/AramaÇubuğu/AramaÇubuğu.js";
 import Gönderiler from "./bileşenler/Gönderiler/Gönderiler.js";
-import Yorumlar from "./bileşenler/Yorumlar/Yorumlar.js";
-import sahteVeri from "./sahte-veri.js";
-// Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
-// sahteVeri'yi import edin
-import './App.css';
 
+import sahteVeri from "./sahte-veri.js";
 
 const App = () => {
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-	
-	const [gonderiler, setGonderiler] = useState(sahteVeri);
-	const [arama, setArama] = useState("");
 
-  const gonderiyiBegen = gonderiID => {
+  const [gonderiler, setGonderiler] = useState(sahteVeri);
+  const [arama, setArama] = useState(null);
+
+  const gonderiyiBegen = (gonderiID) => {
     /*
       Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
-
       Uygulamanın durumu, React ağacının en üstünde bulunur, ancak iç içe geçmiş bileşenlerin stateleri değiştirememesi adil olmaz!
       Bu fonksiyon, belirli bir gönderinin beğeni sayısını artırılmasına olanak sağlamak amacıyla iç içe geçmiş bileşenlere aktarılır.
-
 	  "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
       `map` içine iletilen callback aşağıdaki mantığı gerçekleştirir:
         - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
         - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
      */
-        const [gonderiler, setGonderiler] = useState(sahteVeri);
-        const [arama, setArama] = useState("");
-      
-        const gonderiyiBegen = (gonderiID) => {
-          setGonderiler(
-           gonderiler.map((post) => 
-             post.id === gonderiID ? (post.likes = post.likes +1) : post
-            )
-          );
-        };
 
+    /*setGonderiler(
+      gonderiler.map((post) => (post.id === gonderiID ? post.likes + 1 : post))
+    );*/
+    /*
+    setGonderiler(
+      gonderiler.map((post) => (post.id === gonderiID ? { ...post, likes: post.likes + 1 } : post))
+    );
+    */
+
+    setGonderiler(
+      gonderiler.map((post) => {
+        if (post.id === gonderiID) {
+          // beğeni sayısını artırma veya azaltma işlemi yapılıyor
+          const updatedLikes = post.likedByUser
+            ? post.likes - 1
+            : post.likes + 1;
+
+          return {
+            ...post,
+            likes: updatedLikes,
+            likedByUser: !post.likedByUser, // beğenildiği/ beğenilmediği durumu ters çeviriliyor
+          };
+        } else {
+          return post;
+        }
+      })
+    );
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       {
-        <><AramaÇubuğu /><Gönderiler gonderiyiBegen={gonderiyiBegen} gonderiler={gonderiler} /></>
-     }
+        <>
+          <AramaÇubuğu />
+          <Gönderiler gonderiyiBegen={gonderiyiBegen} gonderiler={gonderiler} />
+        </>
+        /* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */
+      }
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
     </div>
   );
